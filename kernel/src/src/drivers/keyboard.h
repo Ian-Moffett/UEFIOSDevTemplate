1b52462376bc2d36281837ea32e5c9481b540063
Ian-Moffett <hscorp99906@gmail.com>
2022-03-06 18:18:20 -0500

    ..

--- kernel/src/src/drivers/keyboard.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-
-__attribute__((interrupt)) void kb_isr(int_frame_t*);
-
-
-#endif
--- kernel/src/src/util/helpers.h
+++
@@ -1,6 +0,0 @@
-#ifndef HELPERS_H
-#define HELPERS_H
-
-#define HALT __asm__ __volatile__("cli; hlt")
-
-#endif
