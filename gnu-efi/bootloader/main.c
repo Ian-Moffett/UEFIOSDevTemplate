13cbb74a597ab78b8e7bf4900c242ff37027a50a
Ian-Moffett <hscorp99906@gmail.com>
2022-03-07 12:32:57 -0500

    Added helper.

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -1,3 +1,12 @@
 #include <efi.h>
 #include <efilib.h>
 
+
+int memcmp(const void* aptr, const void* bptr, size_t n) {
+	const unsigned char* a = aptr, *b = bptr;
+	for (size_t i = 0; i < n; i++) {
+		if (a[i] < b[i]) return -1;
+		else if (a[i] > b[i]) return 1;
+	}
+	return 0;
+}
